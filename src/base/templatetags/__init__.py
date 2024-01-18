b3288c65a2d002dc774944b0bd8ae06cef6720bc
Viktor1385 <viktor13851998@gmail.com>
2024-01-18 17:47:25 +0200

    fix tabs block

---
+++ src/base/templatetags/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""
+Base tags module.
+"""
---
+++ src/base/templatetags/gallery_tags.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Gallery tags.
+"""
+from django import template
+from wagtail.images.models import Image
+
+register = template.Library()
+
+
+# Retrieves a single gallery item and returns a gallery of images
+@register.inclusion_tag("tags/gallery.html", takes_context=True)
+def gallery(context, gallery):
+    """
+    Gallery.
+    """
+    images = Image.objects.filter(collection=gallery)
+
+    return {
+        "images": images,
+        "request": context["request"],
+    }
