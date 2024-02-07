c13daa932779616c01ce70e955490cbce6550c6c
Viktor1385 <viktor13851998@gmail.com>
2024-02-07 10:09:54 +0200

    change benefits block, add max num of blocks

--- components/benefits_block/benefits_block.py
+++ components/benefits_block/benefits_block.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from wagtail.blocks import CharBlock, RichTextBlock, StructBlock
+from wagtail.blocks import CharBlock, ListBlock, RichTextBlock, StructBlock
 from wagtail.images.blocks import ImageChooserBlock
 
 
@@ -12,9 +12,11 @@ class BenefitBlock(StructBlock):
 class BenefitsBlock(StructBlock):
     title = CharBlock()
     subtitle = CharBlock(required=False)
-    benefit_1 = BenefitBlock()
-    benefit_2 = BenefitBlock()
-    benefit_3 = BenefitBlock()
+    benefits = ListBlock(BenefitBlock, min_num=0, max_num=3)
+
+    # benefit_1 = BenefitBlock()
+    # benefit_2 = BenefitBlock()
+    # benefit_3 = BenefitBlock()
 
     class Meta:
         template = "benefits_block/benefits_block.html"
